e60c9501bf777fcbd200c04e3b7a245cc1bf8cc8
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-02-18 01:00:30 +0530

    feat: data model added

---
+++ SwiftUI-MapApp/Model/location.swift
@@ -0,0 +1,22 @@
+//
+//  location.swift
+//  SwiftUI-MapApp
+//
+//  Created by Prashanna Rajbhandari on 18/02/2023.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location{
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
+
+

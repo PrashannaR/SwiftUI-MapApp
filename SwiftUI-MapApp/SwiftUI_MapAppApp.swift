d253066f553d316846aee134b10939bfe62eca86
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-02-18 00:50:10 +0530

    feat: initial commit

---
+++ SwiftUI-MapApp/SwiftUI_MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapAppApp.swift
+//  SwiftUI-MapApp
+//
+//  Created by Prashanna Rajbhandari on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
